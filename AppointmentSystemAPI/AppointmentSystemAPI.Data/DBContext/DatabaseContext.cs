352c8c9be5e9010e2ae88cffda1a751fe16a5a55
Khairi Norizan <mknrzn@KhairiNorizan.mediacom.info>
2022-01-17 02:51:21 -0600

    Dbcontext and department mapping

--- AppointmentSystemAPI/AppointmentSystemAPI.Data/DBContext/DatabaseContext.cs
+++ AppointmentSystemAPI/AppointmentSystemAPI.Data/DBContext/DatabaseContext.cs
@@ -1,7 +1,26 @@
+using AppointmentSystemAPI.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
 namespace AppointmentSystemAPI.Data.DBContext
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
-        
+        public virtual DbSet<Booking> Bookings { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Device> Devices { get; set; }
+        public virtual DbSet<Engineer> Engineers { get; set; }
+        public virtual DbSet<Problem> Problems { get; set; }
+        public virtual DbSet<SystemUser> SystemUsers { get; set; }
+        public virtual DbSet<WorkAssigned> WorkAssigns { get; set; }
+        public virtual DbSet<WorkStatus> WorkStatus { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
--- AppointmentSystemAPI/AppointmentSystemAPI.Data/Model/Department.cs
+++ AppointmentSystemAPI/AppointmentSystemAPI.Data/Model/Department.cs
@@ -2,8 +2,8 @@ namespace AppointmentSystemAPI.Data.Model
 {
     public class Department
     {
-        private int DepartmentID { get; set; }
-        private string DepartmentName { get; set; }
-        private string DepartmentFloor { get; set; }
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentFloor { get; set; }
     }
 }
